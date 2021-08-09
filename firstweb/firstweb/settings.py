3ede15cbe990e2c30d4783efda2619731b5a970e
MafuSora <hafizh.maulana.ub@gmail.com>
2021-08-09 12:20:22 +0700

    day3

--- firstweb/firstweb/settings.py
+++ firstweb/firstweb/settings.py
@@ -13,7 +13,7 @@ import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
@@ -120,9 +120,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-# STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
-STATICFILES_ROOT = os.path.join(PROJECT_ROOT, 'static')
-# STATICFILES_DIR = os.path.join(BASE_DIR, "static")
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
+# STATICFILES_ROOT = os.path.join(PROJECT_ROOT, 'static')
+STATICFILES_DIR = os.path.join(BASE_DIR, "static")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field

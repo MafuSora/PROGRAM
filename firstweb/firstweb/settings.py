4d4aafc9dc83c3784d4b36e7aaef5f8a644e7d7a
MafuSora <hafizh.maulana.ub@gmail.com>
2021-08-10 14:24:24 +0700

    commit heroku

--- firstweb/firstweb/settings.py
+++ firstweb/firstweb/settings.py
@@ -124,7 +124,7 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 # STATICFILES_ROOT = os.path.join(PROJECT_ROOT, 'static')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static"), ]
-
+STATICFILES_STORAGE =
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 

0d0797a155646a046e1a468da9f47245db670b64
MafuSora <hafizh.maulana.ub@gmail.com>
2023-10-22 11:18:59 +0700

    aabit change on wsgi

--- firstweb/firstweb/wsgi.py
+++ firstweb/firstweb/wsgi.py
@@ -13,4 +13,12 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'firstweb.settings')
 
+
+# import os
+
+# from django.core.wsgi import get_wsgi_application
+
+# # os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'web_skripsi.settings')
+# os.environ['DJANGO_SETTINGS_MODULE']= 'web_skripsi.settings'
 application = get_wsgi_application()
+app = application
